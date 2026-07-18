0f3cfd9e6af5ee79c69d55bbc8ca03b21b70ff4f
Gagan V <gagan23cs@gmail.com>
2026-07-18 16:32:04 +0530

    Allow frontend CORS

--- backend/src/main/java/com/gagan/mutualfunds/controller/FundController.java
+++ backend/src/main/java/com/gagan/mutualfunds/controller/FundController.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api/fund")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174", "http://localhost:5175"})
+@CrossOrigin(origins = "*")
 public class FundController {
 
     @Autowired
