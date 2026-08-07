141859fa4196a67a32ef3f006a35ef5b73fffb5a
Gagan <162724523+GAGAN2324@users.noreply.github.com>
2026-08-07 13:04:56 +0530

    Delete frontend directory

--- frontend/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default function Loader(){
-    return (
-        <div className="loaderWrap">
-            <div className="neonLoader" />
-        </div>
-    )
-}
--- frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "https://mutualfundsperformanceprediction-production.up.railway.app/api/fund",
-});
-
-export default api;
